e57c2d3ab106f9ef16ca1c3bcb9c93aece5096e4
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2024-01-03 15:34:00 +0100

    design fixes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,46 @@
 <template>
-  <main class="container-fluid">
+  <main class="about">
     <img src="../assets/imgs/shapeOne.svg" alt="colorful shape" id="shapeOne" />
-    <Portrait />
+
     <img src="../assets/imgs/shapeTwo.svg" alt="colorful shape" id="shapeTwo" />
   </main>
 </template>
 
-<style></style>
+<style scoped lang="scss">
+.about {
+  height: 100vh !important;
+  #shapeOne {
+    position: absolute;
+    z-index: -1;
+    width: 750px;
+    top: -100px;
+    left: -150px;
+    @include md {
+      width: 1000px;
+      left: -100px;
+    }
+    @include lg {
+      width: 1100px;
+      left: -170px;
+    }
+  }
+
+  #shapeTwo {
+    position: absolute;
+    top: 600px;
+    left: -350px;
+    width: 800px;
+    z-index: -2;
+    @include md {
+      width: 1100px;
+      left: -100px;
+      rotate: 180deg;
+    }
+    @include lg {
+      width: 1200px;
+      left: 60%;
+      top: 100px;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import { onMounted, onUnmounted } from "vue";
 
 const updateParallax = () => {
   const scrollY = window.scrollY;
+  const windowHeight = window.innerHeight;
   const shapeOne = document.getElementById("shapeOne");
   const shapeTwo = document.getElementById("shapeTwo");
   const portrait = document.getElementById("portrait");
@@ -12,7 +13,23 @@ const updateParallax = () => {
   helloSection.style.transform = `translateY(${scrollY * 1.4}px)`;
   portrait.style.transform = `translateY(${scrollY * 0.2}px)`;
   shapeOne.style.transform = `translateY(${scrollY * 1.7}px)`;
-  shapeTwo.style.transform = `translateY(${scrollY * 0.2}px)`;
+  if (windowHeight > 1400) {
+    shapeTwo.style.transform = `translateY(${scrollY * 0.4}px)`;
+  } else {
+    shapeTwo.style.transform = `translateY(${scrollY * -0.1}px)`;
+  }
+
+  // Fade out effect
+  const startFade = windowHeight * 0.5; // Start fading at half the window height
+  const endFade = windowHeight; // Completely fade out at one window height
+  let opacity = 1;
+
+  if (scrollY > startFade) {
+    opacity = 1 - (scrollY - startFade) / (endFade - startFade);
+    opacity = Math.max(opacity, 0); // Ensure opacity doesn't go below 0
+  }
+
+  helloSection.style.opacity = opacity;
 };
 
 onMounted(() => {
@@ -47,21 +64,30 @@ onUnmounted(() => {
   position: relative;
   overflow: hidden;
   min-height: 200vh;
-
   #portrait {
     height: 300px;
     width: 300px;
     border-radius: 50%;
-    z-index: 3;
+    z-index: 0;
     position: absolute;
     top: 70px;
     left: 40px;
-
     @include lg {
+      left: 44%;
+      top: 140px;
+      width: 400px;
+      height: 400px;
+    }
+
+    @include xl {
       left: 64%;
       top: 140px;
       width: 400px;
       height: 400px;
+    }
+    @include xxxl {
+      left: auto;
+      right: 30%;
     }
   }
 
@@ -69,14 +95,18 @@ onUnmounted(() => {
     position: absolute;
     top: 390px;
     left: 110px;
-    @include lg {
+    @include xxl {
       left: 550px;
       top: 260px;
     }
     h2 {
       font-size: 70px;
       color: white;
-      @include lg {
+      z-index: 4;
+      @include xl {
+        font-size: 70px;
+      }
+      @include xxl {
         font-size: 100px;
       }
       span {
@@ -84,16 +114,25 @@ onUnmounted(() => {
         font-weight: 600;
         color: $text-color-primary;
         @include lg {
+          text-shadow: 0px 0px 6px #e7e7e79a;
           font-size: 100px;
         }
       }
     }
     h4 {
-      position: inherit;
+      position: fixed;
       color: white;
       font-weight: 600;
       top: 350px;
       left: -70px;
+      @include md {
+        left: 200px;
+      }
+      @include xl {
+        left: auto;
+        right: 100px;
+        color: black;
+      }
     }
   }
 
@@ -103,6 +142,10 @@ onUnmounted(() => {
     width: 750px;
     top: -100px;
     left: -150px;
+    @include md {
+      width: 1000px;
+      left: -100px;
+    }
     @include lg {
       width: 1100px;
       left: -170px;
@@ -115,10 +158,19 @@ onUnmounted(() => {
     left: -350px;
     width: 800px;
     z-index: -2;
+    @include md {
+      width: 1100px;
+      left: -100px;
+      rotate: 180deg;
+    }
     @include lg {
       width: 1200px;
       left: 60%;
-      top: 100px;
+      top: 400px;
+    }
+    @include xxxl {
+      left: auto;
+      right: -10%;
     }
   }
 }
